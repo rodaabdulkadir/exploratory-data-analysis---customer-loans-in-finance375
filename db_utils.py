4d96b66635c666ded3f9a88c5924030f8f6385d9
rodaabdulkadir <129794377+rodaabdulkadir@users.noreply.github.com>
2024-01-13 22:25:16 +0000

    Update db_utils.py

--- db_utils.py
+++ db_utils.py
@@ -27,3 +27,19 @@ class RDSDatabaseConnector:
                 print("Disconnected from the database")
         except Exception as e:
             print(f"Error: Unable to disconnect from the database - {str(e)}")
+
+import yaml
+
+# Function to read credentials from YAML file
+def read_credentials(file_path='credentials.yaml'):
+    try:
+        with open(file_path, 'r') as file:
+            credentials = yaml.safe_load(file)
+            return credentials
+    except Exception as e:
+        print(f"Error: Unable to read credentials from {file_path} - {str(e)}")
+        return None
+
+# Example usage:
+credentials = read_credentials()
+print(credentials)
