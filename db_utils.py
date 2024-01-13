57c201695950a3053f192ea7380dbaa3a94f309d
rodaabdulkadir <129794377+rodaabdulkadir@users.noreply.github.com>
2024-01-13 21:37:39 +0000

    Update db_utils.py

--- db_utils.py
+++ db_utils.py
@@ -1,1 +1,29 @@
+import pymysql
 
+class RDSDatabaseConnector:
+    def __init__(self, host, user, password, database):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.connection = None
+
+    def connect(self):
+        try:
+            self.connection = pymysql.connect(
+                host=self.host,
+                user=self.user,
+                password=self.password,
+                database=self.database
+            )
+            print("Connected to the database")
+        except Exception as e:
+            print(f"Error: Unable to connect to the database - {str(e)}")
+
+    def disconnect(self):
+        try:
+            if self.connection:
+                self.connection.close()
+                print("Disconnected from the database")
+        except Exception as e:
+            print(f"Error: Unable to disconnect from the database - {str(e)}")
