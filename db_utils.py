2e316911ce2ad8ecb8774504216090c1d5817711
rodaabdulkadir <129794377+rodaabdulkadir@users.noreply.github.com>
2024-01-13 22:27:26 +0000

    Update db_utils.py

--- db_utils.py
+++ db_utils.py
@@ -1,11 +1,22 @@
 import pymysql
+import yaml
+
+def load_credentials(file_path='credentials.yaml'):
+    try:
+        with open(file_path, 'r') as file:
+            credentials = yaml.safe_load(file)
+            return credentials
+    except Exception as e:
+        print(f"Error: Unable to load credentials from {file_path} - {str(e)}")
+        return None
 
 class RDSDatabaseConnector:
-    def __init__(self, host, user, password, database):
-        self.host = host
-        self.user = user
-        self.password = password
-        self.database = database
+    def __init__(self, credentials):
+        self.host = credentials.get('RDS_HOST')
+        self.user = credentials.get('RDS_USER')
+        self.password = credentials.get('RDS_PASSWORD')
+        self.database = credentials.get('RDS_DATABASE')
+        self.port = credentials.get('RDS_PORT')
         self.connection = None
 
     def connect(self):
@@ -14,7 +25,8 @@ class RDSDatabaseConnector:
                 host=self.host,
                 user=self.user,
                 password=self.password,
-                database=self.database
+                database=self.database,
+                port=self.port
             )
             print("Connected to the database")
         except Exception as e:
@@ -28,18 +40,10 @@ class RDSDatabaseConnector:
         except Exception as e:
             print(f"Error: Unable to disconnect from the database - {str(e)}")
 
-import yaml
-
-# Function to read credentials from YAML file
-def read_credentials(file_path='credentials.yaml'):
-    try:
-        with open(file_path, 'r') as file:
-            credentials = yaml.safe_load(file)
-            return credentials
-    except Exception as e:
-        print(f"Error: Unable to read credentials from {file_path} - {str(e)}")
-        return None
-
 # Example usage:
-credentials = read_credentials()
-print(credentials)
+# credentials = load_credentials()
+# rds_connector = RDSDatabaseConnector(credentials)
+# rds_connector.connect()
+# # Perform operations like fetching data or other tasks
+# rds_connector.disconnect()
+
